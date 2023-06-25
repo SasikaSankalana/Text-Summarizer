eb4f2001618caea64b7dcc1426b548a5163854ba
SasikaSankalana <79664299+SasikaSankalana@users.noreply.github.com>
2023-06-25 14:42:41 +0530

    Implemented Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from textSummarizer.logging import Logger
 
-Logger.info("Starting Logging")
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+    Logger.info(f'>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<')
+    data_ingestion = DataIngestionPipeline()
+    data_ingestion.main()
+    Logger.info(f'>>>>>>>> Stage {STAGE_NAME} completed <<<<<<')
+except Exception as e:
+    Logger.exception(e)
+    raise e
--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -13,7 +13,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml, "r") as yaml_file:
             content = yaml.safe_load(yaml_file)
-            Logger.info("yaml file {path_to_yaml} loaded successfully")
+            Logger.info(f"yaml file {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty or contains invalid")
